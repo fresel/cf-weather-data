f0211e38bedd5b4dd92706d45f8fcdc5326d9c41
Fredrik Selander <fresel72@gmail.com>
2025-09-18 20:59:23 +0200

    Setup default springboot app
    - main app
    - default tests
    - .gitgnore
    - update README

---
+++ backend/cf-weather-api/src/main/java/com/github/fresel/cfwd/api/CfWeatherApi.java
@@ -0,0 +1,13 @@
+package com.github.fresel.cfwd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfWeatherApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfWeatherApi.class, args);
+	}
+
+}
---
+++ backend/cf-weather-api/src/test/java/com/github/fresel/cfwd/api/CfWeatherApiTests.java
@@ -0,0 +1,12 @@
+package com.github.fresel.cfwd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfWeatherApiTests {
+
+	@Test
+	void contextLoads() {}
+
+}
