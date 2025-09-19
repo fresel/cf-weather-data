8e029e7d8e7738872a3ed8fed9d8b084f41192d2
Fredrik Selander <fresel72@gmail.com>
2025-09-19 18:06:53 +0200

    Service functionality

---
+++ backend/cf-weather-api/src/main/java/com/github/fresel/cfwd/api/config/ServiceLayerConfig.java
@@ -0,0 +1,14 @@
+package com.github.fresel.cfwd.api.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for the service layer.
+ *
+ * This class can be used to define beans and other configurations related to the service layer.
+ *
+ */
+@Configuration
+public class ServiceLayerConfig {
+
+}
